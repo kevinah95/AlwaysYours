1e5baa3d9139e97ae26afcd2c78babff5a2bb588
Kevin A. Hernández Rostrán <kevinah95@gmail.com>
2026-03-20 14:49:02 -0600

    style: add spotless and prettier formatters (#2)
    * style: add spotless and prettier formatters
    
    Integrate Spotless and Prettier for code formatting across the project.
    This includes configuration files for Prettier and Spotless, ensuring
    consistent code style and formatting rules. Additionally, update Gradle
    files to include necessary plugins and dependencies for formatting.
    
    * build(task): add spotless:setup task

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,35 +1,51 @@
+/*
+ * Copyright 2026 kevinah95 (Kevin A. Hernández Rostrán)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 rootProject.name = "AlwaysYours"
+
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
-    repositories {
-        google {
-            mavenContent {
-                includeGroupAndSubgroups("androidx")
-                includeGroupAndSubgroups("com.android")
-                includeGroupAndSubgroups("com.google")
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal()
+  repositories {
+    google {
+      mavenContent {
+        includeGroupAndSubgroups("androidx")
+        includeGroupAndSubgroups("com.android")
+        includeGroupAndSubgroups("com.google")
+      }
     }
-}
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    mavenCentral()
+    gradlePluginPortal()
+  }
 }
 
+plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
+
 dependencyResolutionManagement {
-    repositories {
-        google {
-            mavenContent {
-                includeGroupAndSubgroups("androidx")
-                includeGroupAndSubgroups("com.android")
-                includeGroupAndSubgroups("com.google")
-            }
-        }
-        mavenCentral()
+  repositories {
+    google {
+      mavenContent {
+        includeGroupAndSubgroups("androidx")
+        includeGroupAndSubgroups("com.android")
+        includeGroupAndSubgroups("com.google")
+      }
     }
+    mavenCentral()
+  }
 }
 
 include(":composeApp")
+
 include(":androidApp")
